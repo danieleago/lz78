b6b324f5bcbd8c98fd6dbdc621518ecbd2148542
d_agosti <d.agosti08@gmail.com>
2016-10-01 17:13:26 +0200

    Add files: header.c header.h first version about read_header and write header functions

---
+++ header.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include <errno.h> 
+#include <stdint.h>
+
+
+#include "constant.h"
+#include "header.h"
+#include "bit_io.h"
+
+
+int write_header(char *input_file, char* output_file, unsigned long dictionary_size, unsigned long crc) {
+	int i,ret;	
+	unsigned long header_crc = 0;	
+	int fd_input = open(input_file, O_RDONLY);
+	bit_io* bit_output = bit_open(output_file, 1);
+	if(bit_output==NULL){
+		printf("Error open file\n");			     
+		return -1;
+        	}   
+	uint8_t length_name = strlen(input_file) < 255 ? strlen(input_file) : 255;
+	printf("length name: %d\n",length_name);	
+	bit_write(bit_output, 8,(uint64_t)length_name);
+	header_crc = update_crc(header_crc,(char)length_name,1);	
+	for( i=0; i <  length_name ; i++){
+		printf("\nchar to white: %c\n",input_file[i]);
+		printf("char to white: %llu\n",(uint64_t)input_file[i]);		
+		ret = bit_write(bit_output, 8,(uint64_t)input_file[i]);
+        	if (ret ==-1) {
+            		printf("Error writing file name: %s\n",strerror(errno));
+            		return -1;
+        	}
+		header_crc = update_crc(header_crc, input_file[i],1);
+	}
+	printf("dictionary size: \n");
+	uint64_t buf_dict = htole64((uint64_t)dictionary_size);   
+	ret = bit_write(bit_output, 32, buf_dict);
+    	if (ret ==-1) {
+        	printf("Error writing dictionary size: %s\n",strerror(errno));
+        	return -1;
+    	}
+    	header_crc =update_crc(header_crc, (char*) &buf_dict, 4);
+
+	struct stat file_info;
+	if(fstat(fd_input,&file_info)<0){
+		printf("error read size file\n");
+		return -1;
+	}
+	printf("size file\n");
+	uint64_t buf_size = htole64((uint64_t)file_info.st_size);   
+	ret = bit_write(bit_output, 64, buf_size);
+    	if (ret ==-1) {
+        	printf("Error writing file size: %s\n",strerror(errno));
+        	return -1;
+    	}
+    	header_crc =update_crc(header_crc, (char*) &buf_size, 8);
+
+	printf("crc payload\n");
+	uint64_t buffer = htole64((uint64_t)crc);   
+	ret = bit_write(bit_output, 64, buffer);
+    	if (ret ==-1) {
+        	printf("Error writing crc payload: %s\n",strerror(errno));
+        	return -1;
+    	}
+    	header_crc =update_crc(header_crc, (char*) &buffer, 8);
+
+	printf("header crc\n");
+	buffer = htole64((uint64_t)header_crc);   
+	ret = bit_write(bit_output, 64, buffer);
+    	if (ret ==-1) {
+        	printf("Error writing crc header: %s\n",strerror(errno));
+        	return -1;
+    	}
+
+	bit_close(bit_output);
+	return 0;
+}
+
+int read_header(char *input_file) {
+	int i=0,ret;	
+	uint8_t length_name;
+	uint64_t buffer;
+	unsigned long header_crc = 0;	
+	char filename[255];
+	bit_io* bit_input;
+	bit_input = bit_open(input_file, 0);    
+	ret = bit_read(bit_input, 8,(uint64_t*)&length_name);
+        	if (ret != 8) {
+            		printf("Error reading file name\n");
+            		return -1;
+        }
+	header_crc = update_crc(header_crc,(char) length_name,1);
+	for(;i<length_name;i++){
+		ret = bit_read(bit_input, 8,&buffer);
+        	if (ret != 8) {
+            		printf("Error reading file name\n");
+            		return -1;
+        	}
+		filename[i] = (char) buffer;
+		printf("%llu\n",buffer);
+		printf("%c\n",filename[i]);
+		header_crc = update_crc(header_crc, filename[i],1);
+	}
+	filename[length_name]='\0';
+	printf("original name file: %s \n", filename);
+   	
+	ret = bit_read(bit_input, 32, &buffer);
+    	if (ret != 32) {
+		printf("%llu\n",buffer);
+		printf("%d\n",ret);
+        	printf("Error reading dicionary size: %s\n",strerror(errno));
+        	return -1;
+    	}
+    	header_crc =update_crc(header_crc, (char*) &buffer, 4);
+	uint64_t dictionary_size = le64toh((uint64_t)buffer);
+	printf("dictionary size: %llu\n", dictionary_size);
+   
+	ret = bit_read(bit_input, 64, &buffer);
+    	if (ret != 64) {
+        	printf("Error writing file size %s \n",strerror(errno));
+        	return -1;
+    	}
+    	header_crc =update_crc(header_crc, (char*) &buffer, 8);
+	uint64_t file_size = le64toh(buffer);
+	printf("file size: %llu\n", file_size);
+	   
+	ret = bit_read(bit_input, 64, &buffer);
+    	if (ret != 64) {
+        	printf("Error writing file size\n");
+        	return -1;
+    	}
+    	header_crc =update_crc(header_crc, (char*) &buffer, 8);
+	uint64_t original_crc = htole64(buffer);
+	printf("original crc: %llu\n", original_crc);
+	   
+	ret = bit_read(bit_input, 64, &buffer);
+    	if (ret != 64) {
+        	printf("Error writing file size\n");
+        	return -1;
+    	}
+    	uint64_t h_crc = htole64(buffer);
+	printf("header crc read: %llu\n", h_crc);
+	printf("header crc calculated: %lu \n", header_crc);
+	if(h_crc==header_crc) printf("header crc correctly verify\n");
+
+	bit_close(bit_input);
+	return 0;
+} 
---
+++ header.h
@@ -0,0 +1,2 @@
+int write_header(char *input_file, char* output_file, unsigned long dict_size, unsigned long crc);
+int read_header(char *input_file);
