b5acbfb50a6e9b8626680995d5c457e665803aa0
d_agosti <d.agosti08@gmail.com>
2016-10-02 10:07:22 +0200

    Add new code error in constant.h

--- constant.h
+++ constant.h
@@ -2,7 +2,8 @@
 // constant for error_print
 #define ERROR_PARAMITERS		0
 #define ERROR_SIZE				1
-#define ERROR_INPUT_FILE		2
-#define ERROR_OUTPUT_FILE		3	
-#define ERROR_OPEN				4
+#define ERROR_DICT_NOT_REQUIRED	2
+#define ERROR_INPUT_FILE		3
+#define ERROR_OUTPUT_FILE		4	
+#define ERROR_OPEN				5
 //
