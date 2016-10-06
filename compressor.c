3c26e1ee10db3fa8edad605a2e77e37fae5f92df
d_agosti <d.agosti08@gmail.com>
2016-10-06 20:08:33 +0200

    Resolve bug about writing file

--- compressor.c
+++ compressor.c
@@ -10,6 +10,7 @@
 #include "constant.h"
 #include "bit_io.h"
 #include "dictionary_compressor.h"
+#include "lib_crc.h"
 
 int compressor(bit_io* bit_input, bit_io* bit_output, unsigned int dictionary_size){
 
@@ -17,26 +18,29 @@ int compressor(bit_io* bit_input, bit_io* bit_output, unsigned int dictionary_si
 	init_dictionary(dictionary);
 	ENTRY* entry=NULL;
 	ENTRY* new_entry = NULL;
-	
+	unsigned long crc=0;	
 	uint64_t symbol;
 	uint64_t buffer;
 	while(bit_read(bit_input,8,&symbol)>0){
 		printf("symbol %c\n",(char)symbol);
+		crc = update_crc(crc,(char*)&symbol,1);
 		new_entry = find_entry(dictionary, (char)symbol);
 		if(new_entry==NULL){
-			buffer = (int64_t) index_entry(entry);
-			bit_write(bit_output,sizeof(unsigned int), buffer );
+			buffer = htole32(index_entry(entry));
+			bit_write(bit_output,32, buffer );
 			printf("index write %u\n",(unsigned int)buffer);
 			add_entry(dictionary,(char)symbol,entry);
 			new_entry=find_entry(dictionary,(char)symbol);
 		}
 		entry = new_entry;
 	}
-	buffer = index_entry(entry);
-	bit_write(bit_output,sizeof(unsigned int),buffer);
+	buffer = htole32(index_entry(entry));
+	bit_write(bit_output,32,buffer);
 	printf("index write last symbol %u\n",(unsigned int)buffer);
-	bit_write(bit_output,sizeof(unsigned int),0);
+	bit_write(bit_output,32,0);
 	printf("write EOF\n");
+	buffer = htole64(crc);
+	bit_write(bit_output,64,buffer);
 }
 
 
